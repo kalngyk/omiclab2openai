46219c6a5f0ff704e6ed54162626a917dda2d982
Kal Ng <kalngyk@gmail.com>
2023-12-27 14:58:56 +0800

    adapter-lambda

--- svelte.config.js
+++ svelte.config.js
@@ -1,28 +1,21 @@
-import { adapter } from '@sveltekit-lambda-adapter';
+import adapter from '@tessellator/sveltekit-adapter-lambda';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-        adapter: adapter({
+		adapter: adapter({
 			runtime: 'nodejs18.x'
 		}),
 		alias: {
 			$misc: 'src/misc'
 		}
-	}
-
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-  	preprocess: [
+	},
+	preprocess: [
 		vitePreprocess({
 			postcss: true
 		})
 	]
-
 };
 
 export default config;
